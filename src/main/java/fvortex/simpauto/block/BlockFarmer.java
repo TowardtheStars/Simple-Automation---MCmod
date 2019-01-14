d5462a8a42cac6a73671d17fcd4ba7cf49fbc753
TowardtheStars <hty16@mail.ustc.edu.cn>
2019-01-14 20:13:34 +0800

    added tool tips for farmer block

--- src/main/java/fvortex/simpauto/block/BlockFarmer.java
+++ src/main/java/fvortex/simpauto/block/BlockFarmer.java
@@ -6,11 +6,16 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumBlockRenderType;
+import net.minecraft.util.text.translation.I18n;
+import net.minecraft.util.text.translation.LanguageMap;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
+import java.util.List;
 
 public class BlockFarmer extends BlockContainer
 {
@@ -35,4 +40,9 @@ public class BlockFarmer extends BlockContainer
         return EnumBlockRenderType.MODEL;
     }
 
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World player, List<String> tooltip, ITooltipFlag advanced) {
+        super.addInformation(stack, player, tooltip, advanced);
+        tooltip.add(I18n.translateToLocal("tile.farmer.tooltip"));
+    }
 }
